29731b23c4d4744475298b5a00c08c1d247f24fd
Chenura Silva <chenura55@gmail.com>
2025-09-26 00:03:00 +0530

    make it responsive

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -3,12 +3,12 @@ import { Outlet } from "react-router-dom";
 
 export function MainLayout() {
   return (
-    <div className="min-h-screen flex w-full bg-background">
+    <div className="min-h-screen flex w-full bg-gray-100">
       {/* Sidebar */}
       <AppSidebar />
 
       {/* Main Content */}
-      <div className="flex-1 flex flex-col ml-0 sm:ml-64">
+      <div className="flex-1 flex flex-col ml-0 sm:ml-64 transition-all duration-300">
         <main className="flex-1 p-6">
           <Outlet />
         </main>
