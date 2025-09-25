6db958a379f2d554d2f7c08020f191f3960f9c0a
Chenura Silva <chenura55@gmail.com>
2025-09-25 23:43:46 +0530

    make it responsive

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,16 +1,25 @@
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { AppSidebar } from "./AppSidebar";
 import { Outlet } from "react-router-dom";
+import { useState } from "react";
 
 export function MainLayout() {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
   return (
     <SidebarProvider>
-      <div className="min-h-screen flex w-full bg-background">
+      <div className="min-h-screen flex w-full bg-background relative">
+
         {/* Sidebar */}
         <AppSidebar />
 
         {/* Main Content */}
-        <div className="flex-1 ml-64 flex flex-col">
+        <div
+          className={`
+            flex-1 flex flex-col transition-all duration-300
+            ${sidebarOpen ? "ml-64" : "ml-0"} sm:ml-64
+          `}
+        >
           <main className="flex-1 p-6">
             <Outlet />
           </main>
